2f99eee5d3b3cd5c71bd1b7c6c4eaecf9ba2d5b5
Gabriel <gabriel.sancho13@gmail.com>
2020-02-12 17:55:58 +0000

    Otimização de código

--- 2019_2/maratona/classroom/lista01/g.py
+++ 2019_2/maratona/classroom/lista01/g.py
@@ -7,11 +7,9 @@ for _ in range(qtd):
   tam = len(word)
   sq = ceil(sqrt(tam))
 
-  word += "*" * ((sq**2) - tam)
-
   for i in range(sq, 0, -1):
     for j in range(sq):
-      indice = (sq ** 2) - (i) - (j * sq)
-      if word[indice] != "*":
+      indice = (sq ** 2) - i - (j * sq)
+      if indice < tam:
         print(word[indice], end="")
   print("\n", end="")
